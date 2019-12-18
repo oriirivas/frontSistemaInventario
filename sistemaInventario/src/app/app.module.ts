f3e072133039a973db334415f5b2d06523213bf4
ZorD007 <zordlegand@gmail.com>
2019-12-18 00:00:05 -0300

    peo

--- sistemaInventario/src/app/app.module.ts
+++ sistemaInventario/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccesosComponent } from './components/accesos/accesos.component';
+import { PeoComponent } from './components/peo/peo.component';
+import { AdminpersonasComponent } from './components/accesos/adminpersonas/adminpersonas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccesosComponent,
+    PeoComponent,
+    AdminpersonasComponent
   ],
   imports: [
     BrowserModule
